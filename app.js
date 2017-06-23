948563adaf261ad23cfe4d674211b4eb867a02dd
Tapu Codrut <tapu.codrut@gmail.com>
2017-06-23 21:42:31 +0300

    lesson 12

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ new Vue({
         available: false,
         nearby: false,
         error: false,
-        sucess: false
+        sucess: false,
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            {name: 'Ryu', age: 25},
+            {name: 'Yoshi', age: 35},
+            {name: 'Ken', age: 55},
+        ]
     },
     methods: {
         greet: function(time) {
