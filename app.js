82d7332c3de3b006dc5b7279bdb80b1dd4d38046
Tapu Codrut <tapu.codrut@gmail.com>
2017-06-23 17:03:40 +0300

    lesson 3

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ new Vue({
     //this Vue.js instance is associated with vue-app div
     el: '#vue-app',
     data: {
-        name: 'Codrut'
+        name: 'Codrut',
+        job: 'Student'
+    },
+    methods: {
+        greet: function(time) {
+            return 'Good ' + time + ' ' + this.name + '!'
+        }
     }
 });
