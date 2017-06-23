ffd36029c063a0d5f4ae62ce35d69eb887bcef68
Tapu Codrut <tapu.codrut@gmail.com>
2017-06-23 17:35:57 +0300

    lesson 5

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ new Vue({
     data: {
         name: 'Codrut',
         job: 'Student',
+        age: '23',
         website: 'http://codrut.rocks',
-        websiteTag: '<a href="http://codrut.rocks">Personal website 2</a>'
+        websiteTag: '<a href="http://codrut.rocks">Personal website 2</a>',
+        x: 0,
+        y: 0
     },
     methods: {
         greet: function(time) {
             return 'Good ' + time + ' ' + this.name + '!'
+        },
+        add: function(inc) {
+            this.age += inc;
+        },
+        subtract: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
